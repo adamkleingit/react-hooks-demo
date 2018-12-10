530b0d201c5fdf9dba159e8bae4df890d75b2eb7
adam klein <adam@500tech.com>
2018-12-10 18:46:42 +0200

    Prepared for talk

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 
 export const Toggle = ({ title, isOpen, setIsOpen, children }) => (
   <Fragment>
@@ -11,24 +11,5 @@ export const Toggle = ({ title, isOpen, setIsOpen, children }) => (
     {isOpen ? children : null}
   </Fragment>
 );
-
-export const useToggleState = (value = false) => {
-  const [isOpen, setIsOpen] = useState(value);
-  return {
-    isOpen,
-    setIsOpen
-  };
-};
-
-export const UncontrolledToggle = props => {
-  const toggleState = useToggleState(props.defaultIsOpen);
-
-  return (
-    <ControlledToggle
-      {...toggleState}
-      {...omit(["isOpen", "setIsOpen"], props)}
-    />
-  );
-};
 
 export default Toggle;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-// import { Provider } from './hooks/redux.hooks';
 import store from "./store";
 import Home from "./components/Home";
-import Toggle from "./components/Toggle";
 
 import "./styles.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+};
 
 render(<App />, document.getElementById("root"));
